700f63f300dcea6743091ec78bf6ce76b18d13c9
cyrille hamel <cyrille.hamel@protonmail.com>
2018-09-05 15:45:52 -0400

    ajouter des grille d'affichage des famille dans les vues.

--- src/vue/VueFamille.java
+++ src/vue/VueFamille.java
@@ -1,12 +1,26 @@
 package vue;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class VueFamille extends Application {
 
 	@Override
 	public void start(Stage stade) throws Exception {
+		
+		Pane panneau = new Pane();	
+		GridPane grilleFamille = new GridPane();
+		
+		Label valeurNom = new Label("Dupont");
+		grilleFamille.add(new Label("Nom de famille : "), 0, 0);
+		grilleFamille.add(valeurNom, 1, 0);
+	
+		panneau.getChildren().add(grilleFamille);
+		stade.setScene(new Scene(panneau, 400, 400));
 		
 		stade.show();
 		
--- src/vue/VueListeFamille.java
+++ src/vue/VueListeFamille.java
@@ -1,13 +1,33 @@
 package vue;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class VueListeFamille extends Application {
 
 	@Override
 	public void start(Stage stade) throws Exception {
-
+		
+		Pane panneau = new Pane();	
+		GridPane grilleFamille = new GridPane();
+		
+		Label valeurNom = new Label("Dupont");
+		Label valeurNom2 = new Label("Tremblay");
+		
+		grilleFamille.add(new Label("Nom de famille : "), 0, 0);
+		grilleFamille.add(valeurNom, 0, 1);
+		grilleFamille.add(valeurNom2, 0, 2);
+		
+		
+	
+		panneau.getChildren().add(grilleFamille);
+		stade.setScene(new Scene(panneau, 400, 400));
+		
+		
 		stade.show();
 		
 	}
